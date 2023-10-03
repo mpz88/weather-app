84d09479d52d1605c0e3008b7fb2377f53b4a63d
Mario Pacheco Zinn <mpz-88@hotmail.com>
2023-10-03 18:11:13 +0100

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
+import "../styles/App.css";
 
 function App({ location, forecasts }) {
   const { city, country } = location;
   return (
-    <div className="Weather-App">
+    <div className="weather-app">
       <LocationDetails city={city} country={country} />
       <ForecastSummaries forecasts={forecasts} />
     </div>
